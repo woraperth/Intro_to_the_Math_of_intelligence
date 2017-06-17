ae325bd41832d2eaf70f86bc5dfeab3cb1dc1021
Woratana Ngarmtrakulchol <hotwora@gmail.com>
2017-06-17 21:49:01 +1000

    Add data set

---
+++ test.py
@@ -0,0 +1,73 @@
+# -------------------------------------------------
+# == Gradient Descent for House Price Prediction ==
+# Data Source: House Sales in King County, USA (https://www.kaggle.com/harlfoxem/housesalesprediction)
+# Environment: Python 3.6
+# -------------------------------------------------
+
+# Import libraries
+import pandas as pd
+from numpy import *
+
+# -------------------------------------------
+# Data checking
+# -------------------------------------------
+# Read data
+# mydata = pd.read_csv('kc_house_data.csv')
+
+# Get column names
+# print(list(mydata))
+## Output >> ['id', 'date', 'price', 'bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors', 'waterfront', 'view', 'condition', 'grade', 'sqft_above', 'sqft_basement', 'yr_built', 'yr_renovated', 'zipcode', 'lat', 'long', 'sqft_living15', 'sqft_lot15']
+
+# Calculate correlation between each column to price (find the most correlated one = most likely to have accurate prediction)
+# print(mydata.corr())
+## Output >> `sqft_living` is most correlated to price (0.7), so we will use this column to predict price.
+# -------------------------------------------
+# END Data checking
+# -------------------------------------------
+
+# -----------------------------------------------------
+# Gradient Descent code by Matt Nedrich and Siraj Raval
+# -----------------------------------------------------
+# y = mx + b
+# m is slope, b is y-intercept
+def compute_error_for_line_given_points(b, m, points):
+    totalError = 0
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        totalError += (y - (m * x + b)) ** 2
+    return totalError / float(len(points))
+
+def step_gradient(b_current, m_current, points, learningRate):
+    b_gradient = 0
+    m_gradient = 0
+    N = float(len(points))
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
+        m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
+    new_b = b_current - (learningRate * b_gradient)
+    new_m = m_current - (learningRate * m_gradient)
+    return [new_b, new_m]
+
+def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
+    b = starting_b
+    m = starting_m
+    for i in range(num_iterations):
+        b, m = step_gradient(b, m, array(points), learning_rate)
+    return [b, m]
+
+def run():
+    points = genfromtxt("data.csv", delimiter=",")
+    learning_rate = 0.0001
+    initial_b = 0 # initial y-intercept guess
+    initial_m = 0 # initial slope guess
+    num_iterations = 1000
+    print ("Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points)))
+    print ("Running...")
+    [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
+    print ("After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points)))
+
+if __name__ == '__main__':
+    run()
